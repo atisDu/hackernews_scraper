6295d862ad8652376919ad206c362e840d7a3ef0
Atis Dubrovskis <atisdubrovskis@protonmail.com>
2025-12-04 23:32:11 +0200

    Improved efficiency by integrating everything into one loop

--- scraper.py
+++ scraper.py
@@ -8,27 +8,27 @@ def scrape():
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     
-    #Tituliem un linkiem
-    for title_span in soup.select('.titleline > a'):
-       title = title_span.get_text(strip=True) if title_span else 'No title'
-       url = title_span['href']
-       print(title)
-       print(url)
+    #Vienas cilpas risinājums
+    for row in soup.select('tr.athing'):
+        #Tituls un linki
+        title_span = row.select_one('.titleline > a')
+        title = title_span.get_text(strip=True) if title_span else 'No title'
+        url = title_span['href']
 
-    #Punktiem un vecumam
-    for subtitles in soup.select('.subline'):
-        #punkti
-        score_span = subtitles.select_one('[class^="score"]')
+        #subline klase ir nākamjā tr (rindā) tabulā
+        subline_tr = row.find_next_sibling('tr')
+        
+        #punkti 
+        score_span = subline_tr.select_one('[class^="score"]')
         score = score_span.get_text(strip=True) if score_span else '0 points'
-        print(score)
         #  vecums
-        age_span = subtitles.select_one('.age')
+        age_span = subline_tr.select_one('.age')
         # iekšā .get_text ir formāts "3 hours ago", savukārt title klasē ir timestamps ar kkādu id,
         # tāpēc ņemam tikai pirmos 19 simbolus. 
         age = age_span.get('title')[:19] if age_span else 'unknown age'
-        print(age)
-
-
-
+        
+        #visu izprintējam
+        print(title + ' | ' + url + ' | ' + score + ' | ' + age)
+        
 if __name__ == '__main__':
     scrape()
