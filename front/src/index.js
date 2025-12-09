927f709e8ed48a6145749cb09f7705bc14cadd36
Atis Dubrovskis <atisdubrovskis@protonmail.com>
2025-12-09 23:48:18 +0200

    Small bug fix

--- front/src/index.js
+++ front/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App.js.old';
+import App from './App.js'; //Paldies lint reimportingam, viss nobruka jo girbēja .old importēt?!?!?!?!??
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
